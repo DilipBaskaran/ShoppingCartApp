3f215a93913d733180c1e9f19629561d1649c105
Dilipkumar Baskaran <dilipkumar_bk@outlook.com>
2019-10-03 15:52:10 +0530

    ContextConfiguration Added

--- src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
+++ src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,6 +16,7 @@ import com.shoppingcart.config.ShoppingCartAppApplication;
 import com.shoppingcart.config.WebSecurityConfig;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration
 @WebMvcTest(value = ShoppingCartAppApplication.class)
 @Import(value={WebSecurityConfig.class})
 public class ShoppingCartAppApplicationTests {
