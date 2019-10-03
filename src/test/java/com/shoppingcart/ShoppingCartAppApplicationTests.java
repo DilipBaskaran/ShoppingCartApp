bc13bd8a5fb8ecdd5dfa592c193eaff1566dd828
Dilipkumar Baskaran <dilipkumar_bk@outlook.com>
2019-10-03 14:56:08 +0530

    Updated Mock Class

--- src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
+++ src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
@@ -6,7 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,7 +14,7 @@ import com.shoppingcart.config.ShoppingCartAppApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={ShoppingCartAppApplication.class})
-@AutoConfigureMockMvc
+@WebMvcTest
 public class ShoppingCartAppApplicationTests {
 
 	@Autowired
