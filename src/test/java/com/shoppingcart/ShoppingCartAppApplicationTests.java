7d7cc01a41f9642b505d5492265f18d571d79819
Dilipkumar Baskaran <dilipkumar_bk@outlook.com>
2019-10-03 15:48:29 +0530

    Configured Test properly

--- src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
+++ src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
@@ -6,15 +6,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 import com.shoppingcart.config.ShoppingCartAppApplication;
+import com.shoppingcart.config.WebSecurityConfig;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes={ShoppingCartAppApplication.class})
-@AutoConfigureMockMvc
+@WebMvcTest(value = ShoppingCartAppApplication.class)
+@Import(value={WebSecurityConfig.class})
 public class ShoppingCartAppApplicationTests {
 
 	@Autowired
