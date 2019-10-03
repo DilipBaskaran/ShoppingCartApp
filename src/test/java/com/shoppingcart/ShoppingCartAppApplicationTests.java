3bb3d546c9a1d812b3a5277c0b3711b272ae8638
Dilipkumar Baskaran <dilipkumar_bk@outlook.com>
2019-10-03 14:45:56 +0530

    Fix updated for Test Class

--- src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
+++ src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes={ShoppingCartAppApplication.class})
 @AutoConfigureMockMvc
 public class ShoppingCartAppApplicationTests {
 
