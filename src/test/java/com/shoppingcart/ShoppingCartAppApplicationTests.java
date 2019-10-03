f6b55b54f6b0b624fa86cdb47fd0804b2a78da26
Dilipkumar Baskaran <dilipkumar_bk@outlook.com>
2019-10-03 14:50:46 +0530

    import properly updated

--- src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
+++ src/test/java/com/shoppingcart/ShoppingCartAppApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import com.shoppingcart.config.ShoppingCartAppApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={ShoppingCartAppApplication.class})
